301f70bb7fc956332e57ecaba8f98fd5e252ce15
Stuart Sharpe <stu@stuartsharpe.co.uk>
2015-11-07 11:21:25 +0000

    Added Playground file.

---
+++ TDD In Swift Playgrounds.playground/Pages/RomanNumerals.xcplaygroundpage/Contents.swift
@@ -0,0 +1,73 @@
+//: [Previous](@previous)
+
+//: ## Roman Numerals Kata
+//: Kata Description from [Agile Katas](http://agilekatas.co.uk/katas/RomanNumerals-Kata)
+//:
+//: The Romans wrote their numbers using letters; specifically the letters 'I' meaning '1', 'V' meaning '5', 'X' meaning '10', 'L' meaning '50', 'C' meaning '100', 'D' meaning '500', and 'M' meaning '1000'. There were certain rules that the numerals followed which should be observed.
+//:
+//: The symbols 'I', 'X', 'C', and 'M' can be repeated at most 3 times in a row. The symbols 'V', 'L', and 'D' can never be repeated. The '1' symbols ('I', 'X', and 'C') can only be subtracted from the 2 next highest values ('IV' and 'IX', 'XL' and 'XC', 'CD' and 'CM'). Only one subtraction can be made per numeral ('XC' is allowed, 'XXC' is not). The '5' symbols ('V', 'L', and 'D') can never be subtracted.
+//:
+//: ### Examples
+//:
+//: A correct implementation should produce the following roman numerals output for the given arabic inputs:
+//:
+//: * 1 = I
+//: * 5 = V
+//: * 9 = IX
+//: * 10 = X
+//: * 50 = L
+//: * 90 = XC
+//: * 100 = C
+//: * 500 = D
+//: * 1000 = M
+//: * 2499 = MMCDXCIX
+//: * 3949 = MMMCMXLIX
+
+import XCTest
+
+//: ### Implementation
+//: Write your implementation of the roman numeral converter below. You can use classes, structs, enums or any other types.
+
+
+//: ### Tests
+//: Fill in the unit test class to verfiy the functionality of your implementation:
+
+class RomanNumeralConverterTests: XCTestCase {
+    override func setUp() {
+    }
+    
+    override func tearDown() {
+    }
+    
+}
+
+
+class PlaygroundTestObserver : NSObject, XCTestObservation {
+    @objc func testCase(testCase: XCTestCase, didFailWithDescription description: String, inFile filePath: String?, atLine lineNumber: UInt) {
+        print("Test failed on line \(lineNumber): \(testCase.name), \(description)")
+    }
+}
+
+let observer = PlaygroundTestObserver()
+let center = XCTestObservationCenter.sharedTestObservationCenter()
+center.addTestObserver(observer)
+
+
+
+struct TestRunner {
+    
+    func runTests(testClass:AnyClass) {
+        print("Running test suite \(testClass)")
+        let tests = testClass as! XCTestCase.Type
+        let testSuite = tests.defaultTestSuite()
+        testSuite.runTest()
+        let run = testSuite.testRun as! XCTestSuiteRun
+        
+        print("Ran \(run.executionCount) tests in \(run.testDuration)s with \(run.totalFailureCount) failures")
+    }
+    
+}
+
+TestRunner().runTests(RomanNumeralConverterTests)
+
+//: [Next](@next)
---
+++ TDD In Swift Playgrounds.playground/Pages/TDD In Swift Playgrounds.xcplaygroundpage/Contents.swift
@@ -0,0 +1,58 @@
+//: ## TDD In Swift Playgrounds
+//:
+//: Wouldn’t it be really cool if somehow you could write your code in a Playground alongside the unit tests which validate the code. That way, once you’ve finished your class you could move the code and the tests back into your app, and get the best of both worlds!
+
+//: First: make sure you import XCTest:
+import XCTest
+
+//: The actual unit tests look an awful lot like, well, unit tests. For instance, a standard XCTestCase subclass which contains a failing test:
+
+class MyTests : XCTestCase {
+    func testShouldFail() {
+        XCTFail("You must fail to succeed!")
+    }
+    func testShouldPass() {
+        XCTAssertEqual(2+2, 4)
+    }
+}
+
+//: ### Observe & Report
+//:
+//: In order to report filing tests, we also need to add a test observer, conforming to XCTestObservation, which will be notified whenever a test fails and print the failure to the console. The observer is then added to the XCTestObservationCenter to allow the system to notify the observer of test failures:
+
+class PlaygroundTestObserver : NSObject, XCTestObservation {
+    @objc func testCase(testCase: XCTestCase, didFailWithDescription description: String, inFile filePath: String?, atLine lineNumber: UInt) {
+        print("Test failed on line \(lineNumber): \(testCase.name), \(description)")
+    }
+}
+
+let observer = PlaygroundTestObserver()
+let center = XCTestObservationCenter.sharedTestObservationCenter()
+center.addTestObserver(observer)
+
+
+//: ### The Running Man
+//:
+//: After this is the TestRunner, which runs the tests from an XCTestCase and reports on the results. This is done using the same XCTestSuite mechanism which Xcode uses to run unit tests. At the end of the run, a message is printed to the console, telling you how many tests were run, how long it took, and how many of the tests failed.
+
+struct TestRunner {
+    
+    func runTests(testClass:AnyClass) {
+        print("Running test suite \(testClass)")
+        let tests = testClass as! XCTestCase.Type
+        let testSuite = tests.defaultTestSuite()
+        testSuite.runTest()
+        let run = testSuite.testRun as! XCTestSuiteRun
+        
+        print("Ran \(run.executionCount) tests in \(run.testDuration)s with \(run.totalFailureCount) failures")
+    }
+    
+}
+
+//: ### Failure Is Not An Option
+//:
+//: Lastly, call the runTests() function, passing in the test class you wish to run (in this case, 'My Tests'
+
+TestRunner().runTests(MyTests)
+
+//: [Next](@next)
