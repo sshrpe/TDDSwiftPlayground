8faf753055d56d58859a4ce27c479700f399c6b7
sshrpe <stu@stuartsharpe.co.uk>
2019-11-26 09:43:58 +0000

    Update for Xcode 11

--- TDD In Swift Playgrounds - iOS.playground/Sources/TestRunner.swift
+++ TDD In Swift Playgrounds - iOS.playground/Sources/TestRunner.swift
@@ -14,7 +14,7 @@ public struct TestRunner {
     
     public func runTests(testClass:AnyClass) {
         let tests = testClass as! XCTestCase.Type
-        let testSuite = tests.defaultTestSuite()
+        let testSuite = tests.defaultTestSuite
         testSuite.run()
         _ = testSuite.testRun as! XCTestSuiteRun
     }
